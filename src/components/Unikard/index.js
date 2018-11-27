63a72254bb8eff65e2236099eaaaf10c8f1895db
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-11-27 11:07:10 +0100

    init unikname integration

---
+++ src/components/Unikard/index.js
@@ -0,0 +1,84 @@
+import React, { PureComponent } from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { translate } from 'react-i18next'
+import { createStructuredSelector } from 'reselect'
+
+import Box from 'components/base/Box'
+
+
+type Props = {
+
+}
+
+type State = {
+
+}
+
+const mapStateToProps = createStructuredSelector({
+    // TODO map state
+  })
+  
+  const mapDispatchToProps = {
+    // TODO map dispatch
+  }
+
+const style={
+
+    card:{
+
+    },
+
+
+    profile_picture:{
+        'borderRadius': '50%',
+        'height': '45px',
+        'width': '45px',
+        'marginRight': '1rem'
+    }
+
+}
+
+
+class Unikard extends PureComponent<Props, State> {
+    
+    render(){
+        let { unikname } = this.props;
+        
+        let label = unikname.resolver.label;
+        let coinSupportedLabel = label === 'default' ? '' : `by #${label}`;
+
+        let supportedCoins = unikname.supportedTypes.map((type)=>`${type.label}`).join(' - ')
+
+        return (
+            <Box flow={1} style={style.card}>
+                <Box horizontal>
+                <span>
+                    <img style={style.profile_picture} src={unikname.profile.picture}/>
+                </span>
+                    <h1>@{unikname.resolver.unikname}</h1>
+                </Box>
+                <Box horizontal flow={2}>
+                    <h2>Trust:</h2>{unikname.security.trust}/5
+                    <h2>Unik:</h2>{unikname.security.unik ? "true" : "false"}
+                </Box>
+                <Box>
+                    <h2>Coin supported {coinSupportedLabel}</h2>
+                    {supportedCoins}
+                </Box>
+                <span>unik-name.com</span>  
+            </Box>
+        );
+    }
+
+
+}
+
+export default compose(
+    connect(
+      mapStateToProps,
+      mapDispatchToProps,
+    ),
+    translate(),
+  )(Unikard)
+  
--- src/components/modals/Send/fields/RecipientField.js
+++ src/components/modals/Send/fields/RecipientField.js
@@ -7,10 +7,17 @@ import { openURL } from 'helpers/linking'
 import { urls } from 'config/urls'
 import Box from 'components/base/Box'
 import LabelWithExternalIcon from 'components/base/LabelWithExternalIcon'
+import Label from 'components/base/Label'
 import RecipientAddress from 'components/RecipientAddress'
+import Unikard from 'components/Unikard'
+
 import { track } from 'analytics/segment'
 import { createCustomErrorClass } from 'helpers/errors'
 import { CantScanQRCode } from 'config/errors'
+
+import network from 'api/network'
+
+
 
 type Props<Transaction> = {
   t: T,
@@ -31,6 +38,7 @@ class RecipientField<Transaction> extends Component<
     isValid: true,
     warning: null,
     QRCodeRefusedReason: null,
+    unikname:null
   }
   componentDidMount() {
     this.resync()
@@ -85,35 +93,77 @@ class RecipientField<Transaction> extends Component<
     return true
   }
 
+  onBlur = async (event)=>{
+
+    const { bridge,account,transaction, onChangeTransaction } = this.props
+
+    let unikname = event.currentTarget.value;
+    
+    let recepient = unikname.split('#');
+
+    let explicit = recepient[0].substr(1);
+    let label = recepient[1];
+    let type = account.currency.ticker ;
+
+    let promise = network({ method: 'GET', url: `http://localhost:3000/uniknames/${explicit}/labels/${label}/types/${type}` })
+
+    const { data, status } = await promise
+
+    console.log("code",status)
+    console.log("data",data);
+    if (data) {
+      this.setState({ unikname: data })
+      let t = bridge.editTransactionRecipient(account, transaction, data.resolver.address)
+      onChangeTransaction(t)
+    }
+    
+  }
+
   handleRecipientAddressHelp = () => {
     openURL(urls.recipientAddressInfo)
     track('Send Flow Recipient Address Help Requested')
   }
   render() {
-    const { bridge, account, transaction, t, autoFocus } = this.props
-    const { isValid, warning, QRCodeRefusedReason } = this.state
-    const value = bridge.getTransactionRecipient(account, transaction)
+    const { bridge, account, transaction, autoFocus } = this.props
+    const { isValid, warning, QRCodeRefusedReason,unikname } = this.state
+
+    let recipient = bridge.getTransactionRecipient(account, transaction)
+    if( unikname ){
+      recipient = `@${unikname.resolver.unikname}`
+      let label = unikname.resolver.label;
+      if( label !== 'default' ){
+        recipient += `#${label}`
+      }
+    }
+
+    const value = recipient;
 
     const error =
       !value || isValid
         ? QRCodeRefusedReason
         : new InvalidAddress(null, { currencyName: account.currency.name })
 
+    let resolvedAddress = unikname ? <LabelWithExternalIcon label={`Resolved address : ${unikname.resolver.address}`}/> : null;
+    let unikard = unikname ? <Unikard unikname={unikname}/> : null
+
     return (
-      <Box flow={1}>
-        <LabelWithExternalIcon
-          onClick={this.handleRecipientAddressHelp}
-          label={t('send.steps.amount.recipientAddress')}
-        />
-        <RecipientAddress
-          autoFocus={autoFocus}
-          withQrCode
-          error={error}
-          warning={warning}
-          value={value}
-          onChange={this.onChange}
-        />
-      </Box>
+        <Box flow={1}>
+          <LabelWithExternalIcon
+            onClick={this.handleRecipientAddressHelp}
+            label="Recipient @unik-name"
+          />
+          <RecipientAddress
+            autoFocus={autoFocus}
+            withQrCode
+            //error={error}
+            //warning={warning}
+            value={value}
+            onChange={this.onChange}
+            onBlur={this.onBlur}
+          />
+          {resolvedAddress}
+          {unikard}
+        </Box>
     )
   }
 }
