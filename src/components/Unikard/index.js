d63cf41aa127d2afe3b5277c9d39088b890c5fe0
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-12-04 12:06:39 +0100

    add style on unikard

--- src/components/Unikard/index.js
+++ src/components/Unikard/index.js
@@ -6,6 +6,7 @@ import { createStructuredSelector } from 'reselect'
 
 import Box from 'components/base/Box'
 
+import './main.css'
 
 type Props = {
 
@@ -23,22 +24,6 @@ const mapStateToProps = createStructuredSelector({
     // TODO map dispatch
   }
 
-const style={
-
-    card:{
-
-    },
-
-
-    profile_picture:{
-        'borderRadius': '50%',
-        'height': '45px',
-        'width': '45px',
-        'marginRight': '1rem'
-    }
-
-}
-
 
 class Unikard extends PureComponent<Props, State> {
     
@@ -48,25 +33,44 @@ class Unikard extends PureComponent<Props, State> {
         let label = unikname.resolver.label;
         let coinSupportedLabel = label === 'default' ? '' : `by #${label}`;
 
-        let supportedCoins = unikname.supportedTypes.map((type)=>`${type.type}`).join(' - ')
+        let supportedCoins = unikname.supportedTypes.map((type)=>(
+            <span className="type">
+                <img src={type.image}/>
+                <span>{type.type}</span>
+            </span>
+        ))
+
+
+        let stars = [];
+        for( let i = 0; i < 5;i++){
+            stars.push( i < unikname.security.trust ? (<span>★</span>) : (<span>☆</span>))
+        }
 
         return (
-            <Box flow={1} style={style.card}>
-                <Box horizontal>
-                <span>
-                    <img style={style.profile_picture} src={unikname.profile.picture}/>
-                </span>
+            <Box flow={1} className="unikard">
+                <Box horizontal className="header">
+                    <span className="profile_picture" src={unikname.type}>
+                        <img src={unikname.profile.picture}/>
+                    </span>
                     <h1>@{unikname.resolver.unikname}</h1>
                 </Box>
-                <Box horizontal flow={2}>
-                    <h2>Trust:</h2>{unikname.security.trust}/5
-                    <h2>Unik:</h2>{unikname.security.unik ? "true" : "false"}
+                <Box vertical className="body">
+                    <Box horizontal flow={2} className="security">
+                        <div className="trust">
+                            <span>Trust:</span>
+                            <span className="stars">{stars}</span>
+                        </div>
+                    </Box>
+                    <Box vertical className="types">
+                        <h2>Types supported {coinSupportedLabel}</h2>
+                        <div> 
+                            {supportedCoins}
+                        </div>
+                    </Box>
                 </Box>
-                <Box>
-                    <h2>Coin supported {coinSupportedLabel}</h2>
-                    {supportedCoins}
+                <Box className="footer">
+                    <a>unik-name.com</a>  
                 </Box>
-                <span>unik-name.com</span>  
             </Box>
         );
     }
